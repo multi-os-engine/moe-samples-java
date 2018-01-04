c3ba8fd0933c97a1fea2df598059ba0833e978f4
Roland Vigh <roland.vigh@migeran.com>
2018-01-04 14:54:59 +0100

    Added openGLBox sample

---
+++ OpenGLBox/android/src/main/java/org/moe/samples/openglbox/android/SurfaceView.java
@@ -0,0 +1,86 @@
+// Copyright (c) 2015, Intel Corporation
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+// 3. Neither the name of the copyright holder nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package org.moe.samples.openglbox.android;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+
+import org.moe.samples.openglbox.android.R;
+
+import java.io.IOException;
+
+class SurfaceView extends GLSurfaceView {
+
+    private BoxRenderer renderer;
+
+    public SurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setup(context);
+    }
+
+    public SurfaceView(Context context){
+        super(context);
+
+        setup(context);
+    }
+
+    private void setup(Context context) {
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2);
+
+        renderer = new BoxRenderer();
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(renderer);
+
+        // Read in the resource
+        final Bitmap bitmap;
+        try {
+            bitmap = BitmapFactory.decodeStream(context.getAssets().open("moe_logo_square.png"));
+            renderer.setBitmap(bitmap);
+        } catch (IOException e) {
+            Log.e("SurfaceView", "Unable find image", e);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        if (e.getAction() == MotionEvent.ACTION_DOWN)
+            renderer.togglePaused();
+        return true;
+    }
+}
