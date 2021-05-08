3520f8d83e22dee6e4809020be055230eef5622e
Noisyfox <timemanager.rick@gmail.com>
2021-05-08 15:20:58 +1000

    Add a unit test demo

---
+++ Calculator/ios/src/test/java/org/moe/samples/calculator/ios/SimpleTest.java
@@ -0,0 +1,10 @@
+package org.moe.samples.calculator.ios;
+
+import org.junit.Test;
+
+public class SimpleTest {
+    @Test
+    public void testSomething() {
+        System.out.println("This is a test!");
+    }
+}
